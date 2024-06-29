758d33662b343b8b158bc12925e2405ce8eb276e
kunalPisolkar24 <kpisolkar24@gmail.com>
2024-06-29 11:40:31 +0530

    completed dashboard and payments functionality

--- Week8/payTMAssignment/frontend/payTM/src/components/Dashboard.jsx
+++ Week8/payTMAssignment/frontend/payTM/src/components/Dashboard.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import { userState, usersState} from "@/store/atom";
-import { useEffect } from "react";
+import { userState, usersState } from "@/store/atom";
+import { useEffect, useState, useRef } from "react";
 
 import {
   Dialog,
@@ -15,8 +15,8 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import {Label } from "@/components/ui/label";
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
@@ -51,19 +51,20 @@ function Dashboard() {
   useEffect(() => {
     const fetchUserData = async () => {
       const token = localStorage.getItem("token");
-      if(token) {
+      if (token) {
         try {
           const response = await fetch("http://localhost:3000/api/auth/me", {
-            headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` },
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
           });
           if (response.ok) {
             const userData = await response.json();
             setUser(userData);
-          }else {
+          } else {
             console.log("Failed to fetch user data");
           }
-        }
-        catch(error){
+        } catch (error) {
           console.log("Failed to fetch user data");
         }
       }
@@ -78,36 +79,105 @@ function Dashboard() {
       if (searchQuery) {
         const token = localStorage.getItem("token");
         try {
-          const response = await fetch(`http://localhost:3000/api/users?query=${String(searchQuery)}`, {
-            headers: { "Authorization": `Bearer ${token}` },
-          });
+          const response = await fetch(
+            `http://localhost:3000/api/users?query=${String(searchQuery)}`,
+            {
+              headers: { Authorization: `Bearer ${token}` },
+            }
+          );
           if (response.ok) {
             const users = await response.json();
             console.log("users", users);
             setUsers(users);
-          }else {
+          } else {
             console.log("Failed to fetch user data");
           }
-        }
-        catch(error) {
+        } catch (error) {
           console.log("Failed to fetch user data", error);
         }
       }
     };
 
     clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {fetchUsers()}, 500);
+    timeoutId = setTimeout(() => {
+      fetchUsers();
+    }, 500);
 
     return () => {
       clearTimeout(timeoutId);
     };
-
   }, [searchQuery]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
-    setUsers(null);;
-    navigate("/login"); 
+    setUsers(null);
+    navigate("/login");
+  };
+
+  const [paymentData, setPaymentData] = useState({
+    recipientId: "",
+    amount: 0,
+    password: "",
+  });
+
+  const handlePaymentChange = (e) => {
+    setPaymentData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSendMoney = async () => {
+    try {
+      if (paymentData.password !== "confirm") {
+        alert("Please enter valid password");
+        return;
+      }
+
+      if (paymentData.amount <= 0 || !paymentData.password) {
+        alert("Please enter valid amount and password");
+        return;
+      }
+
+      const token = localStorage.getItem("token");
+      const response = await fetch("http://localhost:3000/api/payments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          recipient: paymentData.recipientId,
+          amount: parseFloat(paymentData.amount),
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Payment successful", data);
+        alert("Payment successful");
+
+        const token = localStorage.getItem("token");
+        if (token) {
+          const response = await fetch("http://localhost:3000/api/auth/me", {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          if (response.ok) {
+            const userData = await response.json();
+            setUser(userData);
+          } else {
+            console.log("Failed to fetch updated user data");
+          }
+        }
+      } else {
+        const errorData = await response.json();
+        console.log("Payment failed", errorData.message);
+        alert(`Payment failed: ${errorData.message}`);
+      }
+    } catch (error) {
+      console.error("Error while sending payment:", error);
+      alert(`An error occurred while sending payment: ${error.message}`);
+    }
   };
 
   return (
@@ -187,10 +257,12 @@ function Dashboard() {
                   <DropdownMenuLabel>My Account</DropdownMenuLabel>
                   <DropdownMenuSeparator />
                   <Link to={``}>
-                  <DropdownMenuItem>My Profile</DropdownMenuItem>
+                    <DropdownMenuItem>My Profile</DropdownMenuItem>
                   </Link>
                   <DropdownMenuSeparator />
-                  <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
+                  <DropdownMenuItem onClick={handleLogout}>
+                    Logout
+                  </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
             </div>
@@ -206,85 +278,126 @@ function Dashboard() {
               <DollarSign className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold"> 
+              <div className="text-2xl font-bold">
                 $ {user?.balance ? user.balance : 0}
               </div>
             </CardContent>
           </Card>
 
           <div className="py-8 md:py-[0]">
-          <form className="">
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search users..."
-                value = {searchQuery}
-                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-                onChange = {(e) => setSearchQuery(e.target.value)}
-              />
-            </div>
-          </form>
-        </div>
-        <div className="">
-        </div>
-        
-      <Card x-chunk="dashboard-01-chunk-5">
-        <CardHeader>
-          <CardTitle>Users</CardTitle>
-        </CardHeader>
-        <CardContent className="grid gap-8">
-          {users && users.length > 0 ? (
-            users.map((user) => (
-              <div key={user._id} className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/avatars/01.png" alt="Avatar" /> 
-                  <AvatarFallback>
-                    {user.username.charAt(0).toUpperCase()} {/* Display first letter as capital */}
-                  </AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">{user.username}</p>
-                  <p className="text-sm text-muted-foreground">{user.email}</p>
-                </div>
-              <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline" className="ml-auto ">Send Money</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Make Payment </DialogTitle>
-          <DialogDescription>
-            Make payment to {user.username}
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="amount" className="text-right">
-              Amount
-            </Label>
-            <Input id="amount" value="0" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="password" className="text-right">
-              Password
-            </Label>
-            <Input id="password" placeholder="Enter your password"  className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+            <form className="">
+              <div className="relative">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search users..."
+                  value={searchQuery}
+                  className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
               </div>
-            ))
-          ) : (
-            <p>No users found</p>
-          )}
-        </CardContent>
-      </Card>
+            </form>
+          </div>
+          <div className=""></div>
 
+          <Card x-chunk="dashboard-01-chunk-5">
+            <CardHeader>
+              <CardTitle>Users</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-8">
+              {users && users.length > 0 ? (
+                users.map((user) => (
+                  <div key={user._id} className="flex items-center gap-4">
+                    <Avatar className="hidden h-9 w-9 sm:flex">
+                      <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                      <AvatarFallback>
+                        {user.username.charAt(0).toUpperCase()}{" "}
+                        {/* Display first letter as capital */}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="grid gap-1">
+                      <p className="text-sm font-medium leading-none">
+                        {user.username}
+                      </p>
+                      <p className="text-sm text-muted-foreground">
+                        {user.email}
+                      </p>
+                    </div>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button
+                          variant="outline"
+                          className="ml-auto"
+                          onClick={() =>
+                            setPaymentData({
+                              ...paymentData,
+                              recipientId: user._id,
+                            })
+                          }
+                        >
+                          Send Money
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>Make Payment </DialogTitle>
+                          <DialogDescription>
+                            Make payment to {user.username}
+                          </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                          <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="amount" className="text-right">
+                              Amount
+                            </Label>
+                            <Input
+                              id="amount"
+                              value={paymentData.amount}
+                              onChange={handlePaymentChange}
+                              type="number"
+                              name="amount"
+                              className="col-span-3"
+                            />
+                          </div>
+                          <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="password" className="text-right">
+                              Password
+                            </Label>
+                            <Input
+                              id="password"
+                              name="password"
+                              type="password"
+                              value={paymentData.password}
+                              onChange={handlePaymentChange}
+                              placeholder="Enter your password"
+                              className="col-span-3"
+                            />
+                          </div>
+                        </div>
+                        <DialogFooter>
+                          <Button
+                            type="submit"
+                            onClick={() => {
+                              handleSendMoney(user._id);
+                              setPaymentData({
+                                recipientId: user._id,
+                                amount: 0,
+                                password: "",
+                              });
+                            }}
+                          >
+                            Send Money
+                          </Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </div>
+                ))
+              ) : (
+                <p>No users found</p>
+              )}
+            </CardContent>
+          </Card>
         </main>
       </div>
     </>
