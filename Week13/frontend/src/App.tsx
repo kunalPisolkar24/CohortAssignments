f5b5e9fbeedab355b885453dcc3e095d3c1b92fb
kunalPisolkar24 <kpisolkar24@gmail.com>
2024-08-24 21:15:48 +0530

    added dark mode in shadcn

--- Week13/frontend/src/App.tsx
+++ Week13/frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider"
 export default function App() {
 
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <>
+        <Button>Click Me</Button>
+      </>
+    </ThemeProvider>
   )
 }
 
